0ed4036755344efe4d1533cefd0332c2bd4bcded
zubayerhimel <zhimel007@gmail.com>
2021-06-29 21:48:34 +0600

    added signin page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import "./App.css";
+import { useMemo } from "react";
 import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { blue } from "@material-ui/core/colors";
+import SignIn from "./pages/SignIn.page";
 const customBlue = blue["A400"];
 
-const theme = createMuiTheme({
-  typography: {
-    fontFamily: ["Poppins", "sans-serif"].join(","),
-  },
-  shape: {
-    borderRadius: 12,
-  },
-  overrides: {
-    MuiButton: {
-      root: {
-        borderRadius: 6,
-        letterSpacing: 2,
-      },
-    },
-  },
-  palette: {
-    primary: {
-      main: customBlue,
-    },
-  },
-});
-
 const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        typography: {
+          fontFamily: ["Poppins", "sans-serif"].join(","),
+        },
+        shape: {
+          borderRadius: 12,
+        },
+        overrides: {
+          MuiButton: {
+            root: {
+              borderRadius: 6,
+              letterSpacing: 2,
+            },
+          },
+        },
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+
+          primary: {
+            main: customBlue,
+          },
+        },
+      }),
+    [prefersDarkMode]
+  );
   return (
     <ThemeProvider theme={theme}>
-      <div className="App"></div>
+      <div className="App">
+        <SignIn />
+      </div>
     </ThemeProvider>
   );
 };
