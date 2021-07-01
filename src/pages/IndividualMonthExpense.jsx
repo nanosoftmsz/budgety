3d8fa6a9b8e9440c67067e72b016d3f23c8f0f87
zubayerhimel <zhimel007@gmail.com>
2021-07-02 02:28:45 +0600

    added button to add amount and expenditure

--- src/pages/IndividualMonthExpense.jsx
+++ src/pages/IndividualMonthExpense.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { makeStyles } from "@material-ui/styles";
-import { Container, CssBaseline, Grid, Card, CardContent, Button, Dialog, DialogContent, Typography } from "@material-ui/core";
+import { Container, CssBaseline, Grid, Card, CardContent, Button, Dialog, DialogContent, Typography, Box } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -11,19 +11,33 @@ const useStyles = makeStyles((theme) => ({
   mt: {
     marginTop: theme.spacing(2),
   },
+
+  mr: {
+    marginRight: theme.spacing(2),
+  },
+
+  section: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(4),
+  },
 }));
 export default function IndividualMonthExpense() {
   const classes = useStyles();
   const values = [
-    { money: 25000, name: "Total Earned" },
-    { money: 12500, name: "Total Expense" },
-    { money: 5000, name: "Total Save" },
+    { money: 25000, name: "Total Earned", color: "#2979ff" },
+    { money: 12500, name: "Total Expense", color: "#e91e63" },
+    { money: 5000, name: "Total Save", color: "#00c853" },
   ];
   const { id } = useParams();
   return (
     <div>
       <Container component="main" maxWidth="lg" className={classes.root}>
         <CssBaseline />
+        <Box display="flex" justifyContent="center" mb={4}>
+          <Typography variant="h4" color="primary">
+            January 2020
+          </Typography>
+        </Box>
         <Grid container spacing={4} alignItems="center">
           {values.map((v) => (
             <Grid item xs={12} md={4} key={v.name}>
@@ -31,7 +45,7 @@ export default function IndividualMonthExpense() {
                 <CardContent>
                   <Grid container direction="column" justify="center" alignItems="center">
                     <Typography variant="h3"> {v.money.toLocaleString()} </Typography>
-                    <Typography variant="h6" color="primary">
+                    <Typography variant="h6" style={{ color: `${v.color}` }}>
                       {v.name}
                     </Typography>
                   </Grid>
@@ -39,6 +53,14 @@ export default function IndividualMonthExpense() {
               </Card>
             </Grid>
           ))}
+        </Grid>
+        <Grid container justify="flex-end" className={classes.section}>
+          <Button variant="outlined" color="primary" className={classes.mr}>
+            Add Amount
+          </Button>
+          <Button variant="contained" color="primary" disableElevation>
+            Add Expenditure
+          </Button>
         </Grid>
       </Container>
     </div>
