306c322d6699dba8ee1f0716f70f16a79dbb6cd2
zubayerhimel <zhimel007@gmail.com>
2021-07-03 14:20:42 +0600

    added dashboard card

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,84 @@
 import React from "react";
+import { Container, Grid, Card, CardContent, Typography, Box, CssBaseline } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import MonetizationOnRoundedIcon from "@material-ui/icons/MonetizationOnRounded";
+import ReceiptRoundedIcon from "@material-ui/icons/ReceiptRounded";
+import QueueRoundedIcon from "@material-ui/icons/QueueRounded";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(6),
+  },
+  icon: {
+    fontSize: "3rem",
+  },
+  save: {
+    color: "#64dd17",
+  },
+  owned: {
+    color: "#2979ff",
+  },
+  debt: {
+    color: "#e91e63",
+  },
+}));
 
 export default function Dashboard() {
+  const classes = useStyles();
+
   return (
     <div>
-      <h1>Dashboard</h1>
+      <Container component="main" maxWidth="lg" className={classes.root}>
+        <CssBaseline />
+        <Grid container spacing={4} alignItems="center" justify="center">
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Grid container justify="space-between" alignItems="center">
+                  <Grid item>
+                    <Typography variant="h4">42343</Typography>
+                    <Typography variant="subtitle1">Total Saved</Typography>
+                  </Grid>
+                  <Grid item>
+                    <MonetizationOnRoundedIcon className={clsx(classes.icon, classes.save)} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Grid container justify="space-between" alignItems="center">
+                  <Grid item>
+                    <Typography variant="h4">42343</Typography>
+                    <Typography variant="subtitle1">Total Owned</Typography>
+                  </Grid>
+                  <Grid item>
+                    <QueueRoundedIcon className={clsx(classes.icon, classes.owned)} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} sm={6} md={4}>
+            <Card>
+              <CardContent>
+                <Grid container justify="space-between" alignItems="center">
+                  <Grid item>
+                    <Typography variant="h4">42343</Typography>
+                    <Typography variant="subtitle1">Total Debt</Typography>
+                  </Grid>
+                  <Grid item>
+                    <ReceiptRoundedIcon className={clsx(classes.icon, classes.debt)} />
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
