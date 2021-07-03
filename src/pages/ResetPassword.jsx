a5c5240faa32128fa6acd98b8f9edec7eb03059a
zubayerhimel <zhimel007@gmail.com>
2021-07-03 20:21:11 +0600

    removed unncessary imports

--- src/pages/ResetPassword.jsx
+++ src/pages/ResetPassword.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, CssBaseline, TextField, Typography, Box, Container, Divider, Card, CardContent, Grid } from "@material-ui/core";
+import { Button, CssBaseline, TextField, Typography, Box, Container, Card, CardContent } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 import Copyright from "../components/Copyright";
 
