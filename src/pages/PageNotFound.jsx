1d8c7202af487aa4d052924eb67ffbf93b70e100
zubayerhimel <zhimel007@gmail.com>
2021-07-06 20:24:36 +0600

    added page not found page

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Container, Grid, Button, CssBaseline, Typography, Box } from "@material-ui/core";
+import { Link } from "react-router-dom";
+import not_found from "../assets/img/not_found.svg";
 
 export default function PageNotFound() {
   return (
     <div>
-      <h1>Page not found</h1>
+      <Container component="main" maxWidth="sm" style={{ marginTop: "4rem" }}>
+        <CssBaseline />
+        <Grid container direction="column" alignItems="center" justify="center">
+          <img src={not_found} alt="page not found" style={{ height: "50vh" }} />
+          <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mt={3}>
+            <Typography variant="h5">Sorry. Your requested page is not found!</Typography>
+            <Link to="/">
+              <Button variant="contained" color="primary" style={{ marginTop: "2rem" }}>
+                Take me to home
+              </Button>
+            </Link>
+          </Box>
+        </Grid>
+      </Container>
     </div>
   );
 }
